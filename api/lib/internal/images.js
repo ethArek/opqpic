fd9e18732bd2308ba9cc6223aacde55d38bfe9ee
ethArek <arkadiusz.sidzinka@gmail.com>
2020-02-08 11:02:43 +0100

    var image handle

--- api/lib/internal/images.js
+++ api/lib/internal/images.js
@@ -1,21 +1,17 @@
 const fs = require("fs");
 const getOpqHandler = require("../opq/getOpqHandler");
 
+const OPQ_HANDLE =
+  "8e4dfb806861314e21e2e648d96fdf90070f6c782587c785731bd278cbf6d005d5cfc6add4c51bc072dc370e6d4e05acf451a81cffda7680c360380760b66f18";
+const opqHandler = getOpqHandler(OPQ_HANDLE);
+
 async function uploadImage(file) {
-  const opqHandler = getOpqHandler(
-    "8e4dfb806861314e21e2e648d96fdf90070f6c782587c785731bd278cbf6d005d5cfc6add4c51bc072dc370e6d4e05acf451a81cffda7680c360380760b66f18"
-  );
   const imageMeta = await opqHandler.uploadImage(file);
 }
 
 async function downloadImage(handle) {
-  handle =
-    "8e4dfb806861314e21e2e648d96fdf90070f6c782587c785731bd278cbf6d005d5cfc6add4c51bc072dc370e6d4e05acf451a81cffda7680c360380760b66f18";
-  const opqHandler = getOpqHandler(handle);
   const start = Date.now();
-  const imageFile = await opqHandler.getImageData(
-    "eb5e7a688e67d6327cd399ed16c8e1a9c726cc69821d057ea18a7b84d7f5eb6f1a1bcd841d92e1a33099ba1c0754f353eec667a72e972ae58c860e54f00fe3dc"
-  );
+  const imageFile = await opqHandler.getImageData(handle);
   console.log(Date.now() - start);
   return imageFile;
 }
--- api/lib/opq/OpqHandler.js
+++ api/lib/opq/OpqHandler.js
@@ -27,26 +27,6 @@ class OpqHandler {
   }
 
   async getImageData(handle) {
-    // try {
-    //   console.log("tutaj");
-    //   const start = Date.now();
-    //   const masterKey = handle.slice(handle.length / 2);
-    //   handle = handle.slice(0, handle.length / 2);
-    //   const {
-    //     data: { fileDownloadUrl }
-    //   } = await axios.post(
-    //     "https://broker-1.opacitynodes.com:3000/api/v1/download",
-    //     {
-    //       fileID: handle
-    //     }
-    //   );
-    //   console.log(Date.now() - start);
-    //   const { data: imageBuffer } = await axios.get(fileDownloadUrl + "/file");
-    //   console.log(Date.now() - start);
-    // } catch (err) {
-    //   console.log(err);
-    // }
-
     const downloadHandler = this.masterHandle.downloadFile(handle);
     downloadHandler.startDownload();
 
