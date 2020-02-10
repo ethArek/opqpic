017f02a421caa254012314e5ff8ef8f6367ea71a
roberts98 <robertsidzinka@interia.pl>
2020-02-10 22:33:45 +0100

    Correct build files

---
+++ public/precache-manifest.bc1abc38d290a19113dbd0a4fd1ea3fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fcaa1219d9e7999cf77443a877a3f52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "490b679a63fdb0bb73e2",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "490b679a63fdb0bb73e2",
+    "url": "/static/js/2.261beeff.chunk.js"
+  },
+  {
+    "revision": "227076796969f415b75de8fb1afb3376",
+    "url": "/static/js/2.261beeff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c7a38bd699000bc7387",
+    "url": "/static/js/main.36b829e2.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "3153ffe87fe875914a326f163a321e2a",
+    "url": "/static/media/opq-logo.3153ffe8.svg"
+  },
+  {
+    "revision": "fefec929e2134cd2303e68162653224f",
+    "url": "/static/media/upload.fefec929.svg"
+  }
+]);
